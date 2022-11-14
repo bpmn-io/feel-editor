b336ef8a60aa57f80b39be0d9bcbf5b00f9769d9
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2022-11-14 17:08:25 +0100

    deps: update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
+const commonjs = require('@rollup/plugin-commonjs');
+const json = require('@rollup/plugin-json');
 
-import pkg from './package.json';
+const pkg = require('./package.json');
 const nonbundledDependencies = Object.keys({ ...pkg.dependencies });
 
-export default {
+module.exports = {
   input: 'src/index.js',
   output: [ {
     file: pkg.main,
