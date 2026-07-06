a86699ae30cb11483d544d029d665df58b2e4c64
Jarek Danielak <jarek.danielak@camunda.com>
2026-07-06 14:42:33 +0200

    feat: support engine-version compatibility linting
    Add an optional `engines` editor option and a `setEngines()` method that
    flag built-in functions unavailable in the target engine version.
    
    Related to https://github.com/camunda/camunda-modeler/issues/5735

--- src/lint/index.js
+++ src/lint/index.js
@@ -1,4 +1,26 @@
 import { cmFeelLinter } from '@bpmn-io/feel-lint';
 import { linter } from '@codemirror/lint';
 
-export default [ linter(cmFeelLinter()) ];
+/**
+ * Build the FEEL lint extension. Passing `engines` enables version-compatibility
+ * linting of built-in functions (in addition to the always-on syntax linting).
+ *
+ * `engines` is passed through as-is (e.g. `{ camunda: '8.6' }`); the editor is
+ * agnostic to which engines are checked.
+ *
+ * @param { {
+ *   builtins?: import('../core').Variable[],
+ *   engines?: Record<string, string>,
+ * } } [config]
+ *
+ * @return {import('@codemirror/state').Extension}
+ */
+export default function lintExtension({
+  builtins = [],
+  engines
+} = {}) {
+  return linter(cmFeelLinter({
+    builtins,
+    engines
+  }));
+}
